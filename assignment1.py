9b21a0f8a380df5cc43135a1eda5279ec3857e96
Tej prakash Tak <tejprakashtak@gmail.com>
2025-07-30 09:38:50 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+a=input("Enter the frist number:")
+b=input("Enter the second number:")
+a=int(a)
+b=int(b)
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+
+
+f=input("Enter your first name:")
+l=input("Enter your last name:")
+print("Hello, "+f+l+'!'+"Welcone to python program.")
+
